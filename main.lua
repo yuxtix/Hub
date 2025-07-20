6263678ebc4905390b2a77ce90170baf309d6f61
Doal <130514197+yuxtix@users.noreply.github.com>
2025-07-20 17:59:03 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -141,6 +141,30 @@ local Button = Tab:Button({
 })
 
 local Button = Tab:Button({
+    Title = "Catalog Editor",
+    Desc = "Robara el preview de tu personaje",
+    Locked = false,
+    Callback = function()
+        local Params = {
+	RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
+	SSI = "saveinstance",
+}
+
+local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
+
+local CustomOptions = { 
+	SaveBytecode = true,
+	Object = game:GetService("Players").LocalPlayer.PlayerGui.CatalogGUI.AvatarPreview.ViewportHolder.DraggableNPCVPF.NPC,
+	mode = full,
+	noscripts = true
+	
+	}
+
+synsaveinstance(CustomOptions)
+    end
+})
+
+local Button = Tab:Button({
     Title = "Porximity Prompt 0",
     Desc = "Todos los proximity prompts duraran 0 Segundos",
     Locked = false,
